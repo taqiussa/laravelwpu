da5d8a6bb09def46e1148230821a5dffa7a3c400
taqiussa <taqiussa@gmail.com>
2021-08-20 09:14:40 +0700

    login

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -8,17 +8,30 @@
                 <button class="btn-close" data-bs-dismiss="alert" aria-label="close"></button>
             </div>
         @endif
+        @if (session()->has('loginError'))
+            <div class="alert alert-dismissible alert-danger fade show" role="alert">
+                    {{ session('loginError') }}
+                <button class="btn-close" data-bs-dismiss="alert" aria-label="close"></button>
+            </div>
+        @endif
         <main class="form-signin">
             <h1 class="mb-3 text-center h3 fw-normal">Please Login</h1>
-            <form>
-        
+            <form action="/login" method="POST">
+                @csrf
                 <div class="form-floating">
-                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                    <label for="floatingInput">Email address</label>
+                    <input type="email" class="form-control @error('email')
+                        is-invalid
+                    @enderror" id="email" name="email" placeholder="name@example.com" autofocus required value="{{ old('email') }}">
+                    <label for="email">Email address</label>
+                    @error('email')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
                 <div class="form-floating">
-                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                    <label for="floatingPassword">Password</label>
+                    <input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
+                    <label for="password">Password</label>
                 </div>
                 <button class="w-100 btn btn-lg btn-primary" type="submit">Login</button>
             </form>
